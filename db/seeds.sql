0f910b28d5363d75ae9ec7fcda8ce20131ada425
jonhuynh <jonvictory@gmail.com>
2019-11-16 10:59:24 -0700

    add folder structure and base files from the last hw assignment in cpt 14

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+
+USE burger_db;
+
+INSERT INTO burgers (burger_name, devouredy)
+VALUES ("vanilla", 1);
+
+INSERT INTO burgers (flavor, price, quantity)
+VALUES ("chocolate", 0);
+
+INSERT INTO burgers (flavor, price, quantity)
+VALUES ("strawberry", 1);
